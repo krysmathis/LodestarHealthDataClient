d4e266734d0d69840e64a43db05074ffdfeea7f3
krysmathis <krysmathis@gmail.com>
2018-03-06 10:35:15 -0600

    recentering map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3J5c21hdGhpcyIsImEiOiJjamUyc3RmZ3owbHFjMnhycTdjeDlsNzZ5In0.D1mdaVwx9hmI47dZd0cvRQ';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: -86,
+      lat: 34,
+      zoom: 4.5
+    };
+  }
+
   componentDidMount() {
+
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom
+    });
 
     let targetUrl = 'https://api.lodestarhealthdata.com/api/Facility?latitude=55.5&longitude=45.4';
     
@@ -11,24 +33,55 @@ class App extends Component {
     console.log(window.location.href)
     
     if (window.location.href === "http://localhost:3000/") {
-        targetUrl = "http://localhost:5000/api/Facility/?latitude=55.5&longitude=45.5";
+      targetUrl = "http://localhost:5000/api/Facility/?latitude=55.5&longitude=45.5";
     } 
+    
     fetch (targetUrl)
     .then(result =>(result.json()))
-    .then(data => console.log("data", {"url" :targetUrl, "data": data}));
+    .then(data => {
 
+      this.setState({
+        lng: data[0].long,
+        lat: data[0].lat,
+      });
+
+      const { lng, lat, zoom } = this.state;
+      // recenter the map
+      map.flyTo({center: [lng,lat]});
+      console.log("data", {"url" :targetUrl, "data": data})
+      //map.flyTo = ({center: [lng, lat]});
+   
+    });
+
+
+
+    
+
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+
+
+    
   }
 
+
+  
   render() {
+    const { lng, lat, zoom } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        </div>
+        <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
       </div>
     );
   }
